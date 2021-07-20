91d2ed4635ead0372b880818ea99825873a96b6d
Dobakito <mdobak44@icloud.com>
2021-07-20 17:08:56 -0500

    commit tree 6b38387a698e229bc611871f7d5b25774e7d5ee9

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr){
+  let newArr = []
+  for (var i = 0; i < 2; i++) {
+      newArr.push(arr[i])
+  }
+  return newArr;
+}
+
+const returnLastTwoDrivers = function(arr) {
+  let newArr = []
+  for (var i = arr.length - 2; i < arr.length; i++) {
+      newArr.push(arr[i])
+  }
+  return newArr
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+  return function(num){return multiplier * num;}
+}
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(array, option){
+  return option(array)
+}
